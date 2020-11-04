241134fa38b890eeab1b458e20d839a60e79b216
emmy-chen <echen20@stuy.edu>
2020-11-04 09:43:09 -0500

    added SuperArray() method

--- SuperArray.java
+++ SuperArray.java
@@ -1,4 +1,12 @@
 public class SuperArray {
   private String [] data;
   private int size;
+  public SuperArray() {
+    String[] superArray= new String[10];
+  }
+  //public int size()
+  //public String get(int index)
+  //public String set(int index, String element)
+  //private void grow() / resize()
+  //public boolean add(String element)
 }
