2e4adfa5d8b0c7ddb50a20bf17bf1228e5b2a2e8
emmy-chen <echen20@stuy.edu>
2020-11-04 09:53:21 -0500

    added size() method

--- SuperArray.java
+++ SuperArray.java
@@ -1,10 +1,15 @@
 public class SuperArray {
   private String [] data;
   private int size;
+
   public SuperArray() {
     String[] superArray= new String[10];
   }
-  //public int size()
+
+  public int size() {
+    return (this.size);
+  }
+
   //public String get(int index)
   //public String set(int index, String element)
   //private void grow() / resize()
