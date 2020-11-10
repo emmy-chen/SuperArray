db81e6ee6ff9cd0db057b79ab7e221c20d724334
emmy-chen <echen20@stuy.edu>
2020-11-10 10:46:13 -0500

    added findOverlap() method

--- Demo.java
+++ Demo.java
@@ -1,12 +1,14 @@
 public class Demo{
-  public static void removeDuplicates(SuperArray s){
-    for (int i = 0; i < s.size() - 1; i++) {
-      if (s.get(i).equals(s.get(i + 1))) {
-        s.remove(i + 1);
-        i--;
+  public static void removeDuplicates(SuperArray s) {
+    for  (int i = 0; i < s.size(); i++) {
+      for (int j = i + 1; j < s.size(); j++) {
+        if (s[i] == s[j]) {
+          s.remove(i);
+        }
       }
     }
   }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -16,5 +18,18 @@ public class Demo{
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+  }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    SuperArray dupes = new SuperArray();
+    for (int i = 0; i < a.size(); i++) {
+      if (b.contains(a[i])) {
+        dupes.add(a[i]);
+      }
+    }
+    removeDuplicates(dupes);
+    return (dupes.toString());
+  }
+
 }
